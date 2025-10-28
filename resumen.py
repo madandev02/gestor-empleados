cd763a9ac01676d3a3d2fb9f9736ff706dbbf299
Francesca Carcamo <francarcamolopez97@gmail.com>
2025-10-28 20:56:41 -0300

    Eliminar carpeta sobrante

--- resumen.py
+++ resumen.py
@@ -1,5 +1,5 @@
 from datos import nombre, edad, cargo
 from bono import total
 def mostrar_resumen(nombre, edad, cargo, total):
-	print("{nombre}, de {edad} años de edad, que ocupa el cargo de {cargo}, recibirá la cantidad de {total} como suma de su sueldo base y el bono aplicado")
+	print(f"{nombre}, de {edad} años de edad, que ocupa el cargo de {cargo}, recibirá la cantidad de {total} como suma de su sueldo base y el bono aplicado")
     
